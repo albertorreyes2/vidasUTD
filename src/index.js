61ffe8127ac0a8bdee14dfd2866372606f6f45aa
Alberto Reyes <reyesg126@gmail.com>
2021-11-27 18:48:38 -0600

    we have a Index :)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ConfigProvider } from 'antd';
+import esES from 'antd/es/locale/es_ES';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ConfigProvider locale={esES}>
+      <App />
+    </ConfigProvider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
