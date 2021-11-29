15e78e2fdee0be5bf446162e2b8cddd0d9d82936
Alberto Reyes <reyesg126@gmail.com>
2021-11-28 19:46:37 -0600

    getCarreras, get uni, conditions estudiante, end form

--- src/components/formulario/registrar.js
+++ src/components/formulario/registrar.js
@@ -38,16 +38,26 @@ export default function Registrar() {
     const [loading, setLoading] = useState(false);
     const initialState = "";
     const [result, setResult] = useState([]);
-    axios.get("/campaign/getCamps").then(campañas => { console.log('campañas', campañas) })
+    const [carreras, setCarreras] = useState([])
+    const [uni, setUni] = useState([])
+    const handleUni = () => {
+    }
+
+    useEffect(() => {
+        setForm({ ...form, anio_campana: moment().year() })
+        axios.get(`/carreras/getCarreras`).then(res => { setCarreras(res.data.result) });
+        axios.get(`/universidades/getUniversidades`).then(res => { setUni(res.data.result) });
+    }, [])
+
     useEffect(() => {
         window.scrollTo(0, 0);
     }, [])
 
-    const validations = (e) => {
-        let {
-
-        } = form;
-    }
+    // const validations = (e) => {
+    //     let {
+    //         nombre, apellido_p, apellido_m, correo, fecha_nacimiento, cel, estudiante, matricula, resp_nombre, resp_tel, tipo_sangre
+    //     } = form;
+    // }
 
     const handleSearch = (value) => {
         let res = [];
@@ -59,6 +69,26 @@ export default function Registrar() {
         }
         setResult(res);
     };
+
+    const newPredonante = () => {
+        setLoading(true)
+        axios.post("/donador/newDonador", form)
+            .then(function (response) {
+                if (response.data.ok) {
+                    setForm({});
+                    Swal.fire('Nuevo predonante')
+                    formulario.resetFields();
+                    setLoading(false)
+
+                } else {
+                    Swal.fire('No hay predonante')
+                    setLoading(false)
+                }
+            }
+            )
+        setLoading(false)
+    }
+    console.log(form)
     return (
         <>
             <Divider>
@@ -85,7 +115,9 @@ export default function Registrar() {
                             name="nombre"
                             rules={[{ required: true, message: 'Por favor ingresa el nombre ' }]}
                         >
-                            <Input />
+                            <Input onChange={(e) => {
+                                setForm({ ...form, nombre: e.target.value })
+                            }} />
                         </Form.Item>
                     </Col>
                     <Col span={5}>
@@ -94,7 +126,9 @@ export default function Registrar() {
                             name="apellido_p"
                             rules={[{ required: true, message: 'Por favor ingresa el apellido ' }]}
                         >
-                            <Input />
+                            <Input onChange={(e) => {
+                                setForm({ ...form, apellido_p: e.target.value })
+                            }} />
                         </Form.Item>
                     </Col>
                     <Col span={5}>
@@ -102,7 +136,9 @@ export default function Registrar() {
                             label="Apellido materno"
                             name="apellido_m"
                         >
-                            <Input />
+                            <Input onChange={(e) => {
+                                setForm({ ...form, apellido_m: e.target.value })
+                            }} />
                         </Form.Item>
                     </Col>
                 </Row>
@@ -113,7 +149,12 @@ export default function Registrar() {
                             name="fecha_nacimiento"
                             rules={[{ required: true, message: 'Por favor ingresa la fecha de nacimiento' }]}
                         >
-                            <DatePicker style={{ width: "100%", marginRight: "1rem" }} />
+                            <DatePicker style={{ width: "100%", marginRight: "1rem" }}
+                                onChange={(date, fn) => {
+                                    setForm({ ...form, fecha_nacimiento: fn });
+                                }}
+
+                            />
                         </Form.Item>
                     </Col>
                     <Col span={5}>
@@ -124,7 +165,7 @@ export default function Registrar() {
                             { pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, message: "Ingrese email válido " }]}
                         >
                             <AutoComplete
-                                // onChange={(e) => handleChange(e, "correo")}
+                                onChange={(e) => { setForm({ ...form, correo: e }) }}
                                 onSearch={handleSearch}
                                 placeholder="correo@dominio.com"
                             >
@@ -142,71 +183,85 @@ export default function Registrar() {
                             name="cel"
                             rules={[{ required: true, message: 'Por favor ingresa el número de teléfono' }]}
                         >
-                            <InputNumber style={{ width: "100%", marginRight: "1rem" }} />
+                            <InputNumber style={{ width: "100%", marginRight: "1rem" }}
+                                onChange={(e) => {
+                                    setForm({ ...form, cel: e })
+                                }} />
                         </Form.Item>
                     </Col>
                 </Row>
                 <Row justify="left" gutter={[8, 8]}>
                     <Col span={12}>
-                        <Form.Item name="estudiante" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+                        <Form.Item name="estudiante" valuePropName="checked"
+                            onChange={(e) => {
+                                setForm({ ...form, estudiante: e.target.checked === true ? 1 : 0 });
+                            }
+                            }
+                            wrapperCol={{ offset: 8, span: 16 }}>
                             <Checkbox>Estudiante</Checkbox>
                         </Form.Item>
                     </Col>
+
+
                     <Col span={8}>
                         <Form.Item name="tipo_sangre" label="Tipo de sangre" rules={[{ required: true, message: 'Selecciona un campo' }]}>
                             <Select>
-                                <Select.Option value="1">O+</Select.Option>
-                                <Select.Option value="2">A+</Select.Option>
-                                <Select.Option value="3">B+</Select.Option>
-                                <Select.Option value="4">AB+</Select.Option>
-                                <Select.Option value="5">A-</Select.Option>
-                                <Select.Option value="6">B-</Select.Option>
-                                <Select.Option value="7">AB-</Select.Option>
-                                <Select.Option value="8">O-</Select.Option>
-                                <Select.Option value="0">Otra</Select.Option>
+                                <Select.Option value="O+">O+</Select.Option>
+                                <Select.Option value="A+">A+</Select.Option>
+                                <Select.Option value="B+">B+</Select.Option>
+                                <Select.Option value="AB+">AB+</Select.Option>
+                                <Select.Option value="A-">A-</Select.Option>
+                                <Select.Option value="B-">B-</Select.Option>
+                                <Select.Option value="AB-">AB-</Select.Option>
+                                <Select.Option value="O-">O-</Select.Option>
+                                <Select.Option value="Desconocida">Desconocida</Select.Option>
                             </Select>      </Form.Item>
                     </Col>
                 </Row>
-                <Row justify="center" gutter={[8, 8]}>
-                    <Col span={6}>
-                        <Form.Item name="universidad" label="Universidad" rules={[{ required: true, message: 'Selecciona un campo' }]}>
-                            <Select>
-                                <Select.Option value="1">UTD</Select.Option>
-                                <Select.Option value="2">ITD</Select.Option>
-                                <Select.Option value="3">UJED</Select.Option>
-                                <Select.Option value="4">UNID</Select.Option>
-                                <Select.Option value="5">UIM</Select.Option>
-                                <Select.Option value="6">Otra</Select.Option>
-                            </Select>      </Form.Item>
+                {form.estudiante === 1 &&
+                    <Row justify="center" gutter={[8, 8]}>
+                        <Col span={6}>
+                            <Form.Item name="universidad" onClick={handleUni} label="Universidad" rules={[{ required: true, message: 'Selecciona un campo' }]}>
+                                <Select
+                                    placeholder="Selecciona un opcíon"
+                                    onChange={(uni) => {
+                                        setForm({ ...form, id_universidad: uni });
+                                    }}
+                                >
+                                    {uni.map(x => {
+                                        return <Option value={x.id}>{x.nombre}</Option>
+                                    })}
+                                </Select>
+                            </Form.Item>
 
-                    </Col>
-                    <Col span={5}>
-                        <Form.Item name="carrera" label="Carrera" rules={[{ required: true, message: 'Selecciona un campo' }]}>
+                        </Col>
+                        <Col span={5}>
+                            <Form.Item name="carrera" label="Carrera" rules={[{ required: true, message: 'Selecciona un campo' }]}>
 
-                            <Select>
-                                <Select.Option value="1">DN</Select.Option>
-                                <Select.Option value="2">MECA</Select.Option>
-                                <Select.Option value="3">TI</Select.Option>
-                                <Select.Option value="4">OCI</Select.Option>
-                                <Select.Option value="5">MTTO</Select.Option>
-                                <Select.Option value="6">LI</Select.Option>
-                                <Select.Option value="7">DD</Select.Option>
-                                <Select.Option value="8">PI</Select.Option>
-                                <Select.Option value="9">ENRE</Select.Option>
-                                <Select.Option value="0">Otra</Select.Option>
-                            </Select>
-                        </Form.Item>
-                    </Col>
-                    <Col span={5}>
-                        <Form.Item
-                            label="Matricula"
-                            name="matricula"
-                            rules={[{ required: true, message: 'Por favor ingresa la matricula' }]}
-                        >
-                            <Input />
-                        </Form.Item>
-                    </Col>
-                </Row>
+                                <Select
+                                    placeholder="Selecciona un opcíon"
+                                    onChange={(carrera) => {
+                                        setForm({ ...form, id_carrera: carrera });
+                                    }}>
+                                    {carreras.map(x => {
+                                        return <Option value={x.id}>{x.nombre}</Option>
+                                    })}
+                                </Select>
+                            </Form.Item>
+                        </Col>
+                        <Col span={5}>
+                            <Form.Item
+                                label="Matricula"
+                                name="matricula"
+                                rules={[{ required: true, message: 'Por favor ingresa la matricula' }]}
+                            >
+                                <Input onChange={(e) => {
+                                    setForm({ ...form, matricula: e.target.value })
+                                }} />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                }
                 <Row justify="left" gutter={[8, 8]}>
                     <Divider orientation="left">
                         <h3>Contacto de emergencia</h3>
@@ -215,30 +270,25 @@ export default function Registrar() {
                 <Row justify="center" gutter={[8, 8]}>
                     <Col span={8}>
                         <Form.Item name="resp_nombre" label="Contacto de emergencia" rules={[{ required: true, message: 'Selecciona un campo' }]}>
-                            <Input />
+                            <Input onChange={(e) => {
+                                setForm({ ...form, nombre: e.target.value })
+                            }} />
                         </Form.Item>
 
                     </Col>
                     <Col span={8}>
                         <Form.Item name="resp_tel" label="Teléfono de contacto" rules={[{ required: true, message: 'Selecciona un campo' }]}>
-                            <InputNumber style={{ width: "100%", marginRight: "1rem" }}/>
+                            <InputNumber style={{ width: "100%", marginRight: "1rem" }} />
                         </Form.Item>
                     </Col>
                 </Row>
                 <Row gutter={[8, 8]} justify="end">
                     <Col span={8}>
                         <Button
-                        type="primary"
+                            type="primary"
                             icon={<SaveOutlined />}
                             onClick={() => {
-                                validations('positivo')
-                                if (!validations()) {
-                                    message.error(
-                                        "Verifica que todos los campos hayan sido ingresados correctamente."
-                                    );
-                                    setLoading(false);
-                                    return;
-                                }
+                                newPredonante()
                             }}
                             loading={loading}
                         >
